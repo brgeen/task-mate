6375d18f352d11a0d3baa9f5cbb019f6d1a00373
Brooks Geenen <brgeen@gmail.com>
2019-10-18 16:28:14 -0500

    setup POST route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,20 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery Sourced');
+}
+
+function addTask() {
     
+    $('#task-list').empty();
+
+    $.ajax({
+        url: '/list',
+        method: 'POST',
+        data: {
+            task: leftSideOfOperator,
+            complete: false,
+        }
+    }).then(function (response) {
+        appendTasks();
+    });
 }
