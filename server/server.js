9e9e526b31bdd803f7eaefe924edecddbfb2715b
Brooks Geenen <brgeen@gmail.com>
2019-10-18 15:34:35 -0500

    setup server.js with express, pg, body-parser

--- server/server.js
+++ server/server.js
@@ -1,1 +1,35 @@
-console.log('Server JS Sourced');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const pg = require('pg');
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'weekend_to_do_app',
+    max: 10,
+    idleTimeoutMillis: 30000,
+}
+const pool = pg.Pool(config);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('server/public'))
+pool.on('error', (error) => {
+    console.log('Error with Postgres Pool', error);
+})
+
+//--------------------------------------------------------------------------------->
+
+
+// --------------------------------------------------------------------------------->
+
+app.listen(PORT, () => {
+    console.log('Up and running on port', PORT);
+});
+
+
+
+
+
+
+
