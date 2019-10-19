bb99aad01c4bafa1e5c8cfae3668ee3e06ce91e4
Brooks Geenen <brgeen@gmail.com>
2019-10-19 08:41:11 -0500

    setup client side GET and append, setup index for DOM append

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,7 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery Sourced');
+    getTasks();
 }
 
 function addTask() {
@@ -18,4 +19,29 @@ function addTask() {
     }).then(function (response) {
         appendTasks();
     });
-}
+};
+
+function getTasks() {
+
+    $('#task-list').empty();
+
+    $.ajax({
+        url: '/list',
+        method: 'GET',
+    }).then(function (response) {
+        appendTasks(response);
+    });
+};
+
+
+function appendTasks(response) {
+
+    for (let i = 0; i < response.length; i++) {
+        $('#task-list').append(`
+        <li> <data-id="${response[i].id}"> <input type="checkbox">${response[i].task} ${response[i].complete}</li>
+    `);
+    }
+
+    $('#task-input').val('');
+
+};
