710d3b1f9d9e089dacb6aaae4a3358aaa030a929
Brooks Geenen <brgeen@gmail.com>
2019-10-19 14:34:50 -0500

    append items to a table and sort them by ID number

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,14 +3,15 @@ $(document).ready(onReady);
 function onReady() {
     getTasks();
     $('#submit-button').on('click', addTask);
-    $('ul').on('click', '.delete-button', deleteTask)
-    $('ul').on('click', '.complete-button', completeTask)
+    $('tbody').on('click', '.delete-button', deleteTask)
+    $('tbody').on('click', '.complete-button', completeTask)
 }
 
 function deleteTask() {
+
     $.ajax({
         type: 'DELETE',
-        url: `/list/${$(this).parent().data('id')}`,
+        url: `/list/${$(this).parent().parent().data('id')}`,
     }).then(function (response) {
         getTasks();
     }).catch(function (error) {
@@ -22,7 +23,7 @@ function completeTask() {
 
     $.ajax({
         type: 'PUT',
-        url: `/list/true/${$(this).parent().data('id')}`,
+        url: `/list/true/${$(this).parent().parent().data('id')}`,
     }).then(function (response) {
         getTasks();
     }).catch(function (error) {
@@ -57,29 +58,60 @@ function getTasks() {
 };
 
 
-function appendTasks(response) {
+// function appendTasks(response) { // as a list
 
-    for (element of response) {
+//     for (element of response) {
 
-        if (element.complete == true) {
-            $('.task-list').append(`
-            <li class="completed-item" data-id="${element.id}" data-complete="${element.complete}">
-            ${element.task} 
-            <button class="delete-button">Delete</button>
-            <button class="complete-button">Completed</button>
-            </li>
-        `);
+//         if (element.complete == true) {
+//             $('.task-list').append(`
+//             <li class="completed-item" data-id="${element.id}" data-complete="${element.complete}">
+//             ${element.task} 
+//             <button class="delete-button">Delete</button>
+//             <button class="complete-button">Completed</button>
+//             </li>
+//         `);
 
+//         } else {
+//             $('.task-list').append(`
+//             <li data-id="${element.id}" data-complete="${element.complete}">
+//             ${element.task} 
+//             <button class="delete-button">Delete</button>
+//             <button class="complete-button">Completed</button>
+//             </li>
+//         `);
+//         }
+//     }
+//     $('#task-input').val('');
+// };
+
+function appendTasks(response) { // as a table
+    let sortedArray = []; // this array holds the sorted tasks by ID
+
+    for (const element of response) { // this loop takes the response and sorts the items by ID number
+        sortedArray.push([element.id, element.task, element.complete])
+        sortedArray.sort();
+    }
+
+    for (element of sortedArray) { // this loop is for appending the sorted to-do's
+
+        if (element[2] == true) { // this conditional checks to see if the item has been completed, if so, assigns it a completed-item class
+            $('.task-list').prepend(`<tr class="completed-item" data-id="${element[0]}" data-complete="${element[2]}">
+            <td>${element[1]}</td>
+            <td><button class="delete-button">Delete</button></td>
+            <td><button class="complete-button">Completed</button></td>
+            </tr>
+            `);
         } else {
-            $('.task-list').append(`
-            <li data-id="${element.id}" data-complete="${element.complete}">
-            ${element.task} 
-            <button class="delete-button">Delete</button>
-            <button class="complete-button">Completed</button>
-            </li>
-        `);
+            $('.task-list').prepend(`<tr data-id="${element[0]}" data-complete="${element[2]}">
+            <td>${element[1]}</td>
+            <td><button class="delete-button">Delete</button></td>
+            <td><button class="complete-button">Completed</button></td>
+            </tr>
+            `);
         }
     }
     $('#task-input').val('');
 };
+
+
 
