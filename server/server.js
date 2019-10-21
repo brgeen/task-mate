196d8db83346c7e8a7e9acf5498b13bb4965570e
Brooks Geenen <brgeen@gmail.com>
2019-10-21 11:28:35 -0500

    create pool.js file and source it

--- server/server.js
+++ server/server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const pg = require('pg');
-const config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'weekend_to_do_app',
-    max: 10,
-    idleTimeoutMillis: 30000,
-}
-const pool = pg.Pool(config);
+const pool = require('./modules/pool.js');
+// const pg = require('pg');
+// const config = {
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'weekend_to_do_app',
+//     max: 10,
+//     idleTimeoutMillis: 30000,
+// }
+// const pool = pg.Pool(config);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'))
-pool.on('error', (error) => {
-    console.log('Error with Postgres Pool', error);
-})
+// pool.on('error', (error) => {
+//     console.log('Error with Postgres Pool', error);
+// })
 
 //--------------------------------------------------------------------------------->
 
